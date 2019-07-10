8d0d7f857f31ba0c8c77bb4cabc9c76d71f33f42
matthew <mattysmith22@googlemail.com>
2019-07-10 12:01:18 +0100

    Improved DisplayPrint speed.

--- src/DisplayPrint.cpp
+++ src/DisplayPrint.cpp
@@ -1,5 +1,5 @@
 #include "DisplayPrint.h"
-
+#include <vector>
 DisplayPrint::DisplayPrint(Adafruit_SSD1306 &disp) : _display(disp) {}
 
 size_t DisplayPrint::write(uint8_t c) {
@@ -7,4 +7,13 @@ size_t DisplayPrint::write(uint8_t c) {
     _display.write(c);
     _display.display();
     return 1;
+}
+
+size_t DisplayPrint::write(const uint8_t *buffer, size_t size) {
+    for(size_t i = 0; i < size; i++)
+        if(buffer[i] == '\n')
+            _display.write(' ');
+        else
+            _display.write(buffer[i]);
+    _display.display();
 }
--- src/DisplayPrint.h
+++ src/DisplayPrint.h
@@ -8,6 +8,7 @@ class DisplayPrint : public Print {
     public:
     DisplayPrint(Adafruit_SSD1306 &disp);
     size_t write(uint8_t) override;
+    size_t write(const uint8_t *buffer, size_t size) override;
 private:
     Adafruit_SSD1306 &_display;
 };
